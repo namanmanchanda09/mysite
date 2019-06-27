94016f5b6479e837b7343486b7108529519164d6
namanmanchanda09 <naman.mohan09@outlook.com>
2019-06-27 09:02:51 +0530

    Views added and URLs mapped

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello World. You are at the polls index.")
 
+def detail(request, question_id):
+    return HttpResponse("You are looking at question {}.".format(question_id))
+
+def resutls(request, question_id):
+    return HttpResponse("You are looking at the results of question {}".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You are voting onn question {}".format(question_id))
+
+
+
+
